b798a7a17a1477408fca99bd68670305e8863ea6
Tathya Garg <tathya.garg@gmail.com>
2024-05-14 12:00:00 +0530

    Basics

---
+++ flowcharter/nodes/parent.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, height: int, width: int, color: int, text: str):
+        self.children = []
+
+        self.height = height
+        self.width = width
+        self.color = color
+        self.text = text
+
